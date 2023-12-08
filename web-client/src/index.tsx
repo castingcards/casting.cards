fc99ea5e04b350507e177217814d1c81d24de14e
Brian Genisio <Briangenisio@gmail.com>
2023-12-08 17:06:02 -0500

    Adding initialization code for Firebase

--- web-client/src/index.tsx
+++ web-client/src/index.tsx
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Initialize the firebase app
+import './firebaseInit';
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
