b3cc04de7e3753dc15d532c0bd3b65630e836388
Brian Genisio <Briangenisio@gmail.com>
2023-12-28 22:00:28 -0500

    Implemented rudimentary drawing of cards

---
+++ web-client/src/components/games/Card.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import {useDocument} from 'react-firebase-hooks/firestore';
+
+import Paper from "@mui/material/Paper";
+import {styled} from '@mui/material/styles';
+
+import {deckDoc} from "../../firebase-interop/models/deck";
+
+import type {PlayerState} from "../../firebase-interop/models/game";
+
+type Props = {
+    player: PlayerState;
+    scryfallId: string;
+}
+
+const CardPaper = styled(Paper)(({ theme }) => ({
+  width: 80,
+  height: 120,
+  padding: theme.spacing(2),
+  ...theme.typography.body2,
+  textAlign: 'center',
+}));
+
+export function Card({player, scryfallId}: Props) {
+    const [gameResource, loading, error] = useDocument(player.deckId ? deckDoc(player.deckId) : undefined);
+
+    if (error) {
+        return <strong>Error: {JSON.stringify(error)}</strong>;
+    }
+
+    if (loading) {
+        return <span>Loading Deck...</span>
+    }
+
+    const deck = gameResource?.data();
+    const card = deck?.cards.find(card => card.scryfallDetails.id === scryfallId);
+    const imageUrl = card?.imageForCard();
+    return (
+        <CardPaper variant="outlined">
+            <img src={imageUrl} alt={scryfallId} width={80} height={120} />
+        </CardPaper>
+    );
+}
--- web-client/src/components/games/Hand.tsx
+++ web-client/src/components/games/Hand.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 
-import { Paper } from "@mui/material";
+import Paper from "@mui/material/Paper";
 import Typeogrophy from '@mui/material/Typography';
+import List from '@mui/material/Stack';
+import ListItem from '@mui/material/ListItem';
 import {styled} from '@mui/material/styles';
 
+import {Card} from "./Card";
 import type {PlayerState} from "../../firebase-interop/models/game";
 
 const HandCard = styled(Paper)(({ theme }) => ({
@@ -23,6 +26,11 @@ export function Hand({player}: Props) {
     return (
         <HandCard variant="outlined">
             <Typeogrophy variant="body1">Hand ({player.handCardIds.length})</Typeogrophy>
+            <List direction="row" spacing={2}>
+                {player.handCardIds.map(cardId => <ListItem key={cardId}>
+                    <Card player={player} scryfallId={cardId} />
+                </ListItem>)}
+            </List>
         </HandCard>
     );
 }
