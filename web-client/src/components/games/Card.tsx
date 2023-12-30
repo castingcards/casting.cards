360183e5ff2d56a47b070399309572e7e28e0238
MiguelCastillo <manchagnu@gmail.com>
2023-12-30 11:27:35 -0500

    First pass at adding laout to the game board
    This is the first pass at having a layout for different cards on the board.  These changes don't include a commander game type that needs a commander zone and a commander card slot.  That will be done in a follow up change.

--- web-client/src/components/games/Card.tsx
+++ web-client/src/components/games/Card.tsx
@@ -1,9 +1,7 @@
 import React from "react";
 import {useDocument} from 'react-firebase-hooks/firestore';
 
-import Paper from "@mui/material/Paper";
-import {styled} from '@mui/material/styles';
-
+import Grid from '@mui/material/Unstable_Grid2';
 import {deckDoc} from "../../firebase-interop/models/deck";
 
 import type {PlayerState} from "../../firebase-interop/models/playerState";
@@ -13,31 +11,66 @@ type Props = {
     scryfallId: string;
 }
 
-const CardPaper = styled(Paper)(({ theme }) => ({
-  width: 80,
-  height: 120,
-  padding: theme.spacing(2),
-  ...theme.typography.body2,
-  textAlign: 'center',
-}));
+function CardImageLayout({children}: {children: React.ReactNode}) {
+    return (
+        <Grid
+            container
+            width={80}
+            height={120}
+            justifyContent="center"
+            alignContent="center"
+            border="1px solid #AAAAAA"
+            borderRadius="4px"
+        >
+            {children}
+        </Grid>
+    );
+}
 
 export function Card({player, scryfallId}: Props) {
     const [gameResource, loading, error] = useDocument(player.deckId ? deckDoc(player.deckId) : undefined);
 
+    // TODO(miguel): figure out a good way to report errors.  We probably
+    // don't want to just render an error string instead of a card.  Perhaps
+    // we render a place holder card image instead.
+    // Same for the loading state.
     if (error) {
-        return <strong>Error: {JSON.stringify(error)}</strong>;
+        return <ErrorCard>{JSON.stringify(error)}</ErrorCard>;
     }
 
     if (loading) {
-        return <span>Loading Deck...</span>
+        return <LoadingCard/>
     }
 
     const deck = gameResource?.data();
     const card = deck?.cards.find(card => card.scryfallDetails.id === scryfallId);
     const imageUrl = card?.imageForCard();
     return (
-        <CardPaper variant="outlined">
+        <CardImageLayout>
             <img src={imageUrl} alt={scryfallId} width={80} height={120} />
-        </CardPaper>
+        </CardImageLayout>
+    );
+}
+
+export function ErrorCard({children}: {children: React.ReactNode}) {
+    // TODO(miguel): Add a better error card image
+    return (
+        <CardImageLayout>
+            {children}
+        </CardImageLayout>
+    );
+}
+
+export function LoadingCard() {
+    // TODO(miguel): Add a proper loading card image
+    return (
+        <CardImageLayout>Loading...</CardImageLayout>
+    );
+}
+
+export function EmptyCard() {
+    // TODO(miguel): Add a proper empty card image
+    return (
+        <CardImageLayout>Empty</CardImageLayout>
     );
 }
--- web-client/src/components/games/GameBoard.tsx
+++ web-client/src/components/games/GameBoard.tsx
@@ -1,16 +1,113 @@
-import React from "react";
+import * as React from "react";
 import {useDocument} from "react-firebase-hooks/firestore";
-import Box from '@mui/material/Box';
+import Divider from '@mui/material/Divider';
+import Grid from '@mui/material/Unstable_Grid2';
+import Typeogrophy from '@mui/material/Typography';
 
+import {Card, EmptyCard} from "./Card";
 import {Library} from "./Library";
-import {Hand} from "./Hand";
 
-import {playerStateDoc} from "../../firebase-interop/models/playerState";
-import type { Game } from "../../firebase-interop/models/game";
+import {playerStateDoc, PlayerState} from "../../firebase-interop/models/playerState";
+import type {Game} from "../../firebase-interop/models/game";
 
 type Props = {
     game: Game;
     uid: string;
+}
+
+function StackedCardsLayout({
+    cardsId,
+    title,
+    playerState,
+}: {
+    cardsId: Array<string>,
+    title: React.ReactNode,
+    playerState: PlayerState,
+}) {
+    const top = cardsId[cardsId.length - 1];
+
+    return (
+        <Grid container direction="column" alignItems="center">
+            <Typeogrophy variant="body1">{title} ({cardsId.length})</Typeogrophy>
+            <Grid container alignContent="center">
+                <Grid>
+                    {top ? <Card player={playerState} scryfallId={top} />: <EmptyCard/>}
+                </Grid>
+            </Grid>
+        </Grid>
+    );
+}
+
+function Exile({playerState}: {playerState: PlayerState}) {
+    // TODO(miguel): wire in cards that are in the exile bucket
+    const cardsId = playerState.handCardIds;
+
+    return (
+        <StackedCardsLayout title="Exile" cardsId={cardsId} playerState={playerState}/>
+    );
+}
+
+function Graveyard({playerState}: {playerState: PlayerState}) {
+    // TODO(miguel): wire in cards that are in the graveyard bucket
+    const cardsId = playerState.handCardIds;
+
+    return (
+        <StackedCardsLayout title="Graveyard" cardsId={cardsId} playerState={playerState}/>
+    );
+}
+
+function ListCardsLayout({
+    cardsId,
+    title,
+    playerState,
+}: {
+    cardsId: Array<string>,
+    title: React.ReactNode,
+    playerState: PlayerState,
+}) {
+    return (
+        <Grid container direction="column" alignContent="center">
+            <Typeogrophy variant="body1">{title} ({cardsId.length})</Typeogrophy>
+            <Grid container justifyContent="center">
+                {cardsId.length ? cardsId.map((cardId, i) =>
+                    <Grid key={cardId} container justifyContent="center">
+                        <Card player={playerState} scryfallId={cardId} />
+                        {cardsId.length !== 1 && cardsId.length - 1 !== i ?
+                            <Divider
+                                sx={{width: "1em", visibility: "hidden"}}
+                                orientation="horizontal"/> :
+                            null
+                        }
+                    </Grid>
+                ) : <EmptyCard/>}
+            </Grid>
+        </Grid>
+    );
+}
+
+function Lands({playerState}: {playerState: PlayerState}) {
+    // TODO(miguel): either add a new bucket in player state for lands or
+    // out by card type. Needs to take into consideration any spells
+    // that turn things into land.  We need to test if we want _all_ lands
+    // or only lands that have a natural type of land.
+    const cardIds = playerState.battlefieldCardIds;
+    return (
+        <ListCardsLayout title="Lands" playerState={playerState} cardsId={cardIds}/>
+    );
+}
+
+function Battleground({playerState}: {playerState: PlayerState}) {
+    const cardIds = playerState.battlefieldCardIds;
+    return (
+        <ListCardsLayout title="Battleground" playerState={playerState} cardsId={cardIds}/>
+    );
+}
+
+function Hand({playerState}: {playerState: PlayerState}) {
+    const cardIds = playerState.handCardIds;
+    return (
+        <ListCardsLayout title="Hand" playerState={playerState} cardsId={cardIds}/>
+    );
 }
 
 export function GameBoard({game, uid}: Props) {
@@ -25,13 +122,38 @@ export function GameBoard({game, uid}: Props) {
         return <div>Bad bad.</div>;
     }
 
+    // TODO(miguel): wire up custom layouts here where we can swap the
+    // graveyard from left to right. And permanents from top to bottom.
+    // This will be done by simply applying `row` or `row-reverse`
+    // and `column` and `column-reverse` in the grid containers below.
+    // For the graveyard and exile container, you can use column instead
+    // of row and the will be aligned verically instead of horizontally.
+    // We will wire this up later.
+    const graveyardLayout = "column";
+    const permanentCreaturesLayout = "column-reverse";
+
     return (
-        <Box>
+        <>
             <h1>{game.name}</h1>
-            <Box>
-                <Library game={game} player={playerState} />
-                <Hand player={playerState} />
-            </Box>
-        </Box>
+            <Library game={game} player={playerState} />
+            <Grid container
+                direction="row"
+                columns={{ xs: 4, sm: 8, md: 12 }}
+                padding="0 2em"
+            >
+                <Grid container direction={graveyardLayout} justifyContent="center" alignItems="center">
+                    <Exile playerState={playerState}/>
+                    <Divider sx={{width: "1em", visibility: "hidden"}}/>
+                    <Graveyard playerState={playerState}/>
+                </Grid>
+                <Grid container direction="column" flex="1">
+                    <Grid container direction={permanentCreaturesLayout} flex="1">
+                        <Lands playerState={playerState} />
+                        <Battleground playerState={playerState} />
+                    </Grid>
+                    <Hand playerState={playerState} />
+                </Grid>
+            </Grid>
+        </>
     );
 }
